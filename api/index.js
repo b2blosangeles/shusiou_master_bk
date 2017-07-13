5e847a10f37cfeec9cbe8b75489ff865f7c5ddec
b2blosangeles <b2blosangeles@gmail.com>
2017-07-13 10:44:14 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,12 @@
-res.send(env);
+request({
+    url: 'http://api.shusiou.com/api/shusiou_get_default_ad.js',
+    method: "POST",
+    headers: {
+        "content-type": "application/json",
+        },
+    json: {niu:1}
+    }, function (error, resp, body) { 
+      res.send(resp);
+   }
+
+
