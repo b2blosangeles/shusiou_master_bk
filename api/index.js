3a5f46a60f06f0c6a6ada76034ea89bfb459a4a3
b2blosangeles <b2blosangeles@gmail.com>
2017-07-13 10:56:01 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
+function getServerIP() {
+    var ifaces = require('os').networkInterfaces(), address=[];
+    for (var dev in ifaces) {
+        var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
+        for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
+    }
+    return address;
+};
+
 pkg.request({
     url: 'http://api.shusiou.com/api/test_mysql.js',
     method: "POST",
     headers: {
         "content-type": "application/json",
         },
-    json: {niu:1}
+    json: {ip:getServerIP()}
     }, function (error, resp, body) { 
       res.send(typeof body);
    });
