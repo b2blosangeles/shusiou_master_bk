3f0e19f919b8a38bd1f6c2c1b5dd3f871f2e4bae
b2blosangeles <b2blosangeles@gmail.com>
2017-07-13 17:39:15 -0700

    Update update_service.js

--- cron/cron_service/update_service.js
+++ cron/cron_service/update_service.js
@@ -1,6 +1,15 @@
 var path = require('path');
 var env = {root_space:path.join(__dirname, '../../')};
 
+function getServerIP() {
+    var ifaces = require('os').networkInterfaces(), address=[];
+    for (var dev in ifaces) {
+        var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
+        for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
+    }
+    return address;
+};
+
 var diskspace = require(env.root_path + '/package/diskspace/node_modules/diskspace'),
     request = require(env.root_path + '/package/request/node_modules/request');
     
