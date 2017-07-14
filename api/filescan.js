1eb3897457ef32bd9e73d18fa3db57d2a926e8e6
b2blosangeles <b2blosangeles@gmail.com>
2017-07-14 11:34:09 -0700

    Update filescan.js

--- api/filescan.js
+++ api/filescan.js
@@ -16,7 +16,7 @@ function uu(dir, cbk) {
 
     finder.on('file', function (file, stat) {
         total_size += stat.size;
-        _result[_result.length] = {path:file};
+   //     _result[_result.length] = {path:file};
     });
 
     finder.on('link', function (link, stat) {
