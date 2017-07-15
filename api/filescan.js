9803b086052820de14ef1e6f787ec940b3f7efcc
b2blosangeles <b2blosangeles@gmail.com>
2017-07-14 22:44:55 -0700

    Update filescan.js

--- api/filescan.js
+++ api/filescan.js
@@ -22,7 +22,7 @@ function uu(dir, cbk) {
     finder.on('file', function (file, stat) {
        var ff =  ' bytes on both *nix and Windows systems. bytes on both *nix and Windows systems. bytes on both *nix and Windows systems.';
          total_size += stat.size;
-         var filter = new RegExp('/.git/', 'i');
+         var filter = /\/\.git\//;
          if (!filter.test(file)) {
             var patt = new RegExp('^'+ dir, 'i');
             _result[_result.length] = {path:file};
