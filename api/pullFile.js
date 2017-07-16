af4a9c91c3661e745434f6b49eebd7e8fa1c1155
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 06:47:39 -0700

    Update pullFile.js

--- api/pullFile.js
+++ api/pullFile.js
@@ -6,7 +6,7 @@ folderP.build(base, function() {
   var fs = require('fs');
 
   var file = fs.createWriteStream(base+'file.png');
-  var request = http.get('http://api.shusiou.com/api/pipe_stream.js?fn=images/962SfJ00tYM/180_49.png', function(response) {
+  var request = http.get('http://api.shusiou.com/api/pipe_stream.js?video=962SfJ00tYM&fn=images/962SfJ00tYM/180_100.png', function(response) {
         response.pipe(file);
         response.on('end', function() {
           res.sendFile(base + 'file.png');
