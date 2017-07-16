c8f86a36f31e47533e1b760766937c793e6399e9
b2blosangeles <b2blosangeles@gmail.com>
2017-07-15 21:52:23 -0700

    Update pullFile.js

--- api/pullFile.js
+++ api/pullFile.js
@@ -5,10 +5,10 @@ folderP.build(base, function() {
   var http = require('http');
   var fs = require('fs');
 
-  var file = fs.createWriteStream(base+'file.mp4');
+  var file = fs.createWriteStream(base+'file.png');
   var request = http.get('http://api.shusiou.com/api/pipe_stream.js', function(response) {
         response.pipe(file);
   });
-  res.send('/api/inc/folderP/folderP.js niu 3');
+  res.send(base + 'file.png');
 });
 
