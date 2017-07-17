420790f63c3d84f69c22e5f5d20a0c96c5f642f0
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 22:46:29 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -1,8 +1,26 @@
-var exec = require('child_process').exec;
-// var path = require('path');
-// var env = {root_space:path.join(__dirname, '../../')};
-// res.send(env);
-// return true;
+var FOLDERP =  require(env.root_path + '/api/inc/folderP/folderP.js');
+var folderP  = new FOLDERP ();
+var base = '/var/video/';
+folderP.build(base, function() {
+     res.send(base);
+/*    
+    var http = require('http');
+  var fs = require('fs');
+
+  var file = fs.createWriteStream(base+'file.png');
+  var request = http.get('http://api.shusiou.com/api/pipe_stream.js?video=962SfJ00tYM&fn=images/962SfJ00tYM/180_100.png', function(response) {
+        response.pipe(file);
+        response.on('end', function() {
+          res.sendFile(base + 'file.png');
+         // res.send('niu');
+        });
+  });
+//   res.send('niuB');
+ // res.sendFile(base + 'file.png');
+ */
+});
+
+return true;
 
 var request = require(env.root_path + '/package/request/node_modules/request');
 request({
