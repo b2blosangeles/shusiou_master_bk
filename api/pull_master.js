f9acb74464a72f635e38e7ffd1e527fd503c0cc0
b2blosangeles <b2blosangeles@gmail.com>
2017-07-17 06:18:09 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -6,30 +6,6 @@ var base = '/var/video/';
 
 var CP = new pkg.crowdProcess();
 var _f = {};
-
-_f['P1'] = function (cbk) {	
-	request({
-	    url: 'http://api.shusiou.com/api/cloud_resource.report',
-	    method: "GET"
-	    }, function (error, resp, body) { 
-	       cbk(JSON.parse(body));
-	   });	
-},
-_f['P2'] = function (cbk) {	
-	var R = new FOLDER_SCAN();
-	R.scan(base,  '', 
-	function(data) {
-		 cbk(data);
-	}); 
-}
-CP.serial(
-	_f,
-	function(data) {
-		res.send({P1:data.results.P1, P2:data.results.P2});
-	},
-	30000
-);	
-
 var FOLDER_SCAN = function () {
 	var me = this;
 	this.total_size = 0;
@@ -75,3 +51,29 @@ var FOLDER_SCAN = function () {
 
 	};
 };
+
+
+
+_f['P1'] = function (cbk) {	
+	request({
+	    url: 'http://api.shusiou.com/api/cloud_resource.report',
+	    method: "GET"
+	    }, function (error, resp, body) { 
+	       cbk(JSON.parse(body));
+	   });	
+},
+_f['P2'] = function (cbk) {	
+	var R = new FOLDER_SCAN();
+	R.scan(base,  '', 
+	function(data) {
+		 cbk(data);
+	}); 
+}
+CP.serial(
+	_f,
+	function(data) {
+		res.send({P1:data.results.P1, P2:data.results.P2});
+	},
+	30000
+);	
+
