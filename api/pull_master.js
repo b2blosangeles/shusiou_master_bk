377b3e44c898ba8844f26dad472dbd68d631a3d9
b2blosangeles <b2blosangeles@gmail.com>
2017-07-17 11:03:13 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -101,7 +101,7 @@ CP.serial(
 			var request = http.get('http://api.shusiou.com/api/pipe_stream.js?video='+cg[0]['target'], function(response) {
 				response.pipe(file);
 				response.on('end', function() {
-					res.sendFile(cg[0]['target']);
+					res.send(cg[0]['target']);
 				});
 			});		
 		});
