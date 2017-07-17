0effe1a590bdc189e19487bab3b54d611d9eb989
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 22:23:54 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -1,5 +1,4 @@
     var http = require('http');
-    var fs = require('fs');
     var request = http.get('http://api.ahuaiou.com/api/cloud_resource.report', function(response) {
        res.send(response)
     });
