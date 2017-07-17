5a9efaf969171a2470d016c11bdb3e2837b03f39
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 22:26:56 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -1,4 +1,14 @@
-    var http = require('http');
-    var request = http.get('http://api.ahuaiou.com/api/cloud_resource.report', function(response) {
-       res.send(response)
-    });
+var exec = require('child_process').exec;
+var path = require('path');
+var env = {root_space:path.join(__dirname, '../../')};
+
+var request = require(env.root_path + '/package/request/node_modules/request');
+request({
+    url: 'http://api.shusiou.com/api/cloud_resource.report',
+    method: "GET",
+    headers: {
+        "content-type": "application/json",
+        }
+    }, function (error, resp, body) { 
+      res.send(body);
+   });
