0c3b340b1770d033ee0697801e09504df0681bbc
b2blosangeles <b2blosangeles@gmail.com>
2017-07-17 21:56:22 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -72,11 +72,9 @@ CP.serial(
 	function(data) {
 		var P1 = data.results.P1, P2 = data.results.P2, cg=[], rmv=[];
 		
-
 		for (o in P1) {
-			/*
 			for (o_1 in P1[o].list) {
-				
+			/*/	
 				 P1[o].list[o_1]['src'] = P1[o].master.folder + o_1;
 				 P1[o].list[o_1]['file'] = o + '/' + o_1;
 				 var t_dir = path.dirname(base + o + '/' + o_1);
@@ -84,9 +82,9 @@ CP.serial(
 				 P1[o].list[o_1]['t_dir'] = t_dir;
 				
 				 cg[cg.length] = P1[o].list[o_1];
-				 
+			*/	 
 			} 
-			 */
+			 
 		}
 		for (o in P2) {
 			// var v = P2[o]; 
