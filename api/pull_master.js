82d7c9d395d1cce656e800a17a40970968185ceb
b2blosangeles <b2blosangeles@gmail.com>
2017-07-18 12:38:31 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -82,7 +82,7 @@ CP.serial(
 		var P1 = data.results.P1, P2 = data.results.P2, cg=[], rmv=[];	
 
 		for (o in P1) {
-			if ((P1[o].master.size) && (P1[o].master.size != P1[o].master.master_video])) {
+			if ((P1[o].master.size) && (P1[o].master.size != P2[P1[o].master.master_video])) {
 				cg[cg.length] = o  + '/video/video.mp4';
 			}
 			for (o_1 in P1[o].list) {
