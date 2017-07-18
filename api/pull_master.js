2e894c8843bb6a71fc6c7c670c506dfcfbc1e9f9
b2blosangeles <b2blosangeles@gmail.com>
2017-07-18 00:07:44 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -5,7 +5,7 @@ var FOLDERP =  require(env.root_path + '/api/inc/folderP/folderP.js');
 var request = require(env.root_path + '/package/request/node_modules/request');
 
 var folderP  = new FOLDERP ();
-var base = '/var/';
+var base = '/var/video/';
 
 var CP = new pkg.crowdProcess();
 var _f = {};
