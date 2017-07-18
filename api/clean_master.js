96d97d73f19858511bdf36babcf824d2112b1601
b2blosangeles <b2blosangeles@gmail.com>
2017-07-18 15:19:27 -0700

    Update clean_master.js

--- api/clean_master.js
+++ api/clean_master.js
@@ -1,6 +1,5 @@
-var exec = require('child_process').exec;
 var base = '/var/video/';
 
-exec('ls -l ', function(error, stdout, stderr) {
+pkg.exec('ls -l ', function(error, stdout, stderr) {
     res.send('rm -fr A');
 });
