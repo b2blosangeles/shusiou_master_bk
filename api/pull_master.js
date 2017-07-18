8b17dc06011bea1c891daf916b0ffd81281857b8
b2blosangeles <b2blosangeles@gmail.com>
2017-07-18 12:36:51 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -82,10 +82,9 @@ CP.serial(
 		var P1 = data.results.P1, P2 = data.results.P2, cg=[], rmv=[];	
 
 		for (o in P1) {
-			if (((P1[o].master.size) && (P1[o].master.size != P1[o].master.master_video])) {
+			if ((P1[o].master.size) && (P1[o].master.size != P1[o].master.master_video])) {
 				cg[cg.length] = o  + '/video/video.mp4';
 			}
-	
 			for (o_1 in P1[o].list) {
 				if (P1[o].list[o_1] != P2[o + '/' + o_1]) {
 					cg[cg.length] = P1[o].master.code  + '/' + o_1;	 
