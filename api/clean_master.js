780c10f127c78fb174f13bb8b7cffa1108dba4be
b2blosangeles <b2blosangeles@gmail.com>
2017-07-18 15:14:15 -0700

    Update clean_master.js

--- api/clean_master.js
+++ api/clean_master.js
@@ -1,6 +1,26 @@
+/*
+var spawn = require('child_process').spawn,
+
 var exec = require('child_process').exec;
 var base = '/var/video/';
 
 exec('ls -l ' + base, function(error, stdout, stderr) {
     res.send('rm -fr A');
+});
+*/
+
+var util  = require('util'),
+    spawn = require('child_process').spawn,
+    ls    = spawn('ls', ['-lh', '/usr']);
+ 
+ls.stdout.on('data', function (data) {
+  res.send('stdout: ' + data);
+});
+ 
+ls.stderr.on('data', function (data) {
+  res.send('stderr: ' + data);
+});
+ 
+ls.on('exit', function (code) {
+   res.send('child process exited with code ' + code);
 });
