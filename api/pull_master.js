00aea307623d1099fae7174805b0926d1d3e17d8
b2blosangeles <b2blosangeles@gmail.com>
2017-07-18 08:57:18 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -64,7 +64,7 @@ _f['P2'] = function (cbk) {
 	var R = new FOLDER_SCAN();
 	R.scan(base,  '', 
 	function(data) {
-		 cbk(data.list);
+		 cbk(data);
 	}); 
 }
 function existFile(P1, fn) {
@@ -79,6 +79,9 @@ CP.serial(
 	_f,
 	function(data) {
 		var P1 = data.results.P1, P2 = data.results.P2, cg=[], rmv=[];	
+		res.send(P2);
+		return true;
+		
 		for (o in P1) {
 			cg[cg.length] = o  + '/video/video.mp4';	
 			for (o_1 in P1[o].list) {
