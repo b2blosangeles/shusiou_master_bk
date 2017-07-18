bd5356364dafb849c9fddd74b83fdbc2cc1840f9
b2blosangeles <b2blosangeles@gmail.com>
2017-07-17 22:53:02 -0700

    Update pull_master.js

--- api/pull_master.js
+++ api/pull_master.js
@@ -87,8 +87,6 @@ CP.serial(
 			var http = require('http');
 			var fs = require('fs');
 			
-			res.send('http://api.shusiou.com/api/pipe_stream.js?video='+cg[0]);
-			return true;
 			var file = fs.createWriteStream(base + cg[0]);
 			var request = http.get('http://api.shusiou.com/api/pipe_stream.js?fn='+cg[0], function(response) {
 				response.pipe(file);
