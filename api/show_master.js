09ee0cabd3dc9289e91a6bc0b55fde621e087425
b2blosangeles <b2blosangeles@gmail.com>
2017-07-19 08:33:36 -0700

    Update show_master.js

--- api/show_master.js
+++ api/show_master.js
@@ -64,7 +64,7 @@ _f['P0'] = function (cbk) {
 	});	
 }
 _f['P1'] = function (cbk) {	
-	request({
+	pkg.request({
 		url: 'http://api.shusiou.com/api/cloud_resource.report',
 		method: "POST",
 		headers: { "content-type": "application/json"},
