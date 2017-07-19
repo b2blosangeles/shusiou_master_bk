240342ad40adb2c07680498a9013a9b5e787988c
b2blosangeles <b2blosangeles@gmail.com>
2017-07-18 17:41:57 -0700

    Update pull_master.js

--- cron/cron_service/pull_master.js
+++ cron/cron_service/pull_master.js
@@ -7,7 +7,7 @@ var FOLDERP =  require(env.root_path + '/api/inc/folderP/folderP.js');
 var request = require(env.root_path + '/package/request/node_modules/request');
 
 var folderP  = new FOLDERP ();
-var base = '/var/video/';
+var base = '/var/video/', base_ctl = '/var/video_ctl/';
 
 var CP = new crowdProcess();
 var _f = {};
@@ -51,7 +51,9 @@ var FOLDER_SCAN = function () {
 
 _f['P0'] = function (cbk) {
 	folderP.build(base, function() {
-		cbk(true);
+		folderP.build(base_ctl, function() {
+			cbk(true);
+		});	
 	});	
 }
 _f['P1'] = function (cbk) {	
