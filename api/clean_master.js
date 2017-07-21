dcfed8138b9d3cc90b2b5eb2ed1714f60c19f7bc
b2blosangeles <b2blosangeles@gmail.com>
2017-07-21 05:44:54 -0700

    Update clean_master.js

--- api/clean_master.js
+++ api/clean_master.js
@@ -1,6 +1,6 @@
 var base = '/var/video/', base_ctl = '/var/video_ctl/';
 
-pkg.exec('rm -fr ' + base, function(error, stdout, stderr) {
+pkg.exec('rm -fr ' + base_ctl, function(error, stdout, stderr) {
     pkg.exec('rm -fr ' + base, function(error, stdout, stderr) {
          res.send('rm  --- ' + base_ctl);
     });
