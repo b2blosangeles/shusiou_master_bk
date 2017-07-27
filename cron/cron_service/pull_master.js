3d3a9b3a50c00954e20163380dd633ab95d24f93
b2blosangeles <b2blosangeles@gmail.com>
2017-07-27 15:12:01 -0700

    Update pull_master.js

--- cron/cron_service/pull_master.js
+++ cron/cron_service/pull_master.js
@@ -139,7 +139,7 @@ CP.serial(
 	_f,
 	function(data) {
 		console.log('test4')
-		retur true;
+		return true;
 		var P1 = data.results.P1, P2 = data.results.P2, cg=[], rmv=[];
 		for (o in P1) {
 			if ((P1[o].master.size) && (P1[o].master.size != P2[o + '/' + P1[o].master.master_video])) {
