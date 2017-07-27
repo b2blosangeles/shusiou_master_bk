d8ac91c14d728d2f49b7d1bbea8026c3c87c8277
b2blosangeles <b2blosangeles@gmail.com>
2017-07-27 14:54:17 -0700

    Update command.js

--- api/command.js
+++ api/command.js
@@ -1,3 +1,3 @@
-pkg.exec('ls -l', function(error, stdout, stderr) {
-	 res.send(env.root_path + '/cron/cron_service/update_service.js');
+pkg.exec('nodejs ' env.root_path + '/cron/cron_service/update_service.js', function(error, stdout, stderr) {
+	 res.send(stdout);
 });
