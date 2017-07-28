2c2115ec353e54a52c29e76b409c93de670672e8
b2blosangeles <b2blosangeles@gmail.com>
2017-07-28 16:39:27 -0700

    Update server_monitor.js

--- api/server_monitor.js
+++ api/server_monitor.js
@@ -1,3 +1,13 @@
+var base = '/var/video/',  base_ctl = '/var/video_ctl/'
+
+function checkPullLog(v, cbk) {
+	pkg.fs.appendFile(base_ctl + 'pull_log.data', , function(err, stats){
+	    res.send(stats);
+	});
+};
+
+/*
 pkg.exec('git pull ' + env.root_path + '', function(error, stdout, stderr) {
 	 res.send(stdout);
 });
+*/
