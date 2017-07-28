0efee7554a61c1271eeefcd66be6cc332ca1f643
b2blosangeles <b2blosangeles@gmail.com>
2017-07-28 16:53:21 -0700

    Update server_monitor.js

--- api/server_monitor.js
+++ api/server_monitor.js
@@ -1,11 +1,10 @@
 var base = '/var/video/',  base_ctl = '/var/video_ctl/';
- res.send('stats');
-return true;
+
 pkg.fs.stat(base_ctl + 'pull_log.data', function(err, stats){
     if (err) {
 	     res.send(err.message);
     } else {
-	    res.send(stats);
+	    res.send(new Date(stats.mtime).getTime());
     }    
 });
 
