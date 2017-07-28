f37b2f2e8fdd0b4918378e0360a5cc4ecaa1194f
b2blosangeles <b2blosangeles@gmail.com>
2017-07-28 16:42:55 -0700

    Update server_monitor.js

--- api/server_monitor.js
+++ api/server_monitor.js
@@ -1,10 +1,8 @@
 var base = '/var/video/',  base_ctl = '/var/video_ctl/'
 
-function checkPullLog(v, cbk) {
-	pkg.fs.appendFile(base_ctl + 'pull_log.data', , function(err, stats){
-	    res.send(stats);
-	});
-};
+pkg.fs.appendFile(base_ctl + 'pull_log.data', function(err, stats){
+    res.send(stats);
+});
 
 /*
 pkg.exec('git pull ' + env.root_path + '', function(error, stdout, stderr) {
