fc036c7532d2573b5b7e8bbb22f3845d94e30d3e
b2blosangeles <b2blosangeles@gmail.com>
2017-07-29 07:10:43 -0700

    Update cmd.js

--- api/cmd.js
+++ api/cmd.js
@@ -1,3 +1,3 @@
-pkg.exec('git pull ' + env.root_path + '/cron/cron_service/pull_master.js', function(error, stdout, stderr) {
+pkg.exec('git pull ' + env.root_path, function(error, stdout, stderr) {
 	 res.send(stdout);
 });
