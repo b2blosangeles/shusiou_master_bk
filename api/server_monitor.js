5e9fa830020060885331e80424d9e244cdb5211e
b2blosangeles <b2blosangeles@gmail.com>
2017-07-28 23:05:29 -0700

    Update server_monitor.js

--- api/server_monitor.js
+++ api/server_monitor.js
@@ -6,12 +6,20 @@ pkg.fs.stat(base_ctl + 'pull_log.data', function(err, stats){
     } else {
 	    var td = new Date().getTime(), md = new Date(stats.mtime).getTime(), cd = new Date(stats.ctime).getTime(),
 		cv = td - cd, mv = td - md;
-	  //  if (mv > 300000) {
+	    res.send(cv+'---'+mv);
+	    return true;
+	    
+	    if (cv > 3600 * 24 * 1000) {
+		pkg.exec('rm  base_ctl + 'pull_log.data ' + base_ctl + 'rep_log.data', function(error, stdout, stderr) {
+			res.send('clean daily');
+		});		    
+	    }	    
+	    if (mv > 300000) {
 		pkg.exec('git pull ' + env.root_path + '', function(error, stdout, stderr) {
 			pkg.exec('shutdown -r +1', function(error, stdout, stderr) {
 				 res.send('Server will be reboot in 1 minute!');
 			});
 		});		    
-	   // }
+	    }
     }    
 });
