9ddd6cd2a43cbe1b0e15f37a41f76e66d1627758
b2blosangeles <b2blosangeles@gmail.com>
2017-08-11 15:22:09 -0700

    Update cahce_shusiou_g_tts.js

--- api/cahce_shusiou_g_tts.js
+++ api/cahce_shusiou_g_tts.js
@@ -15,6 +15,9 @@ var fn = folder_base + sh.unique(str+'_'+lang)+'.mp3';
 
 var CP = new pkg.crowdProcess();
 
+res.send(fn);
+return true;
+
 var _f = {};
 _f['S1'] = function(cbk) {
 	var folderP = require(env.root_path + '/api/inc/folderP/folderP');
