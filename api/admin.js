f727fa017d0a7a807a4c87d7d39bd8aa053740fe
b2blosangeles <b2blosangeles@gmail.com>
2017-10-09 14:50:32 -0700

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -1,8 +1,21 @@
-var opt = req.param('opt');
-
-var exec = require('child_process').exec;
-exec('ls -l /tmp', function(error, stdout, stderr) {
-   res.send(stdout);
-});
+var opt = req.param('opt'), code = req.param('code');
+if (!code) {
+   res.send('missing code!!');
+} else {   
+   switch(opt) {
+      case 'show':
+         res.sendFile('/tmp/'+code);
+         break;
+      case 'remove':
+         var exec = require('child_process').exec;
+         var cmd = 'rm  /tmp/'+ code;
+         exec(cmd, function(error, stdout, stderr) {
+             res.send(cmd + ' :: ' + stdout);
+           });
+         break;
+      default:
+         res.send('wrong opt!!');
+   }
+}
 
 //res.sendFile('/tmp/shusiou_git.log');
