9409436bfe9b3b686ef49a2d7a1be268b4b8e3de
b2blosangeles <b2blosangeles@gmail.com>
2017-10-09 14:57:17 -0700

    Update git_service.js

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -5,7 +5,7 @@ var env = {root_space:path.join(__dirname, '../../')};
 var cmd = 'cd ' + env.root_space + ' && git pull';
 
 exec(cmd, function(error, stdout, stderr) {
-  var log_str = '--- ' + new Date().toString() + '   ---' + "\n" + cmd + stdout + "\n\n";
+  var log_str = '--- ' + new Date().toString() + '   ---' + "\n" + cmd + "==>\n" + stdout + "\n\n";
   fs.appendFile("/tmp/cron_git.log", log_str, function(err) {
   });   
 });
