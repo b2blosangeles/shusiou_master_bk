736e199b9b482912028ac3ff6fe0e748920fd3ed
b2blosangeles <b2blosangeles@gmail.com>
2017-10-09 14:36:33 -0700

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -1,4 +1,8 @@
 var opt = req.param('opt');
-res.send(opt);
+
+var exec = require('child_process').exec;
+exec('ls -l /tmp', function(error, stdout, stderr) {
+   res.send(stdout);
+});
 
 //res.sendFile('/tmp/shusiou_git.log');
