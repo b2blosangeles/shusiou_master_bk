a0abde3c63eb6b8176af3061b7c34aadd2b41679
b2blosangeles <b2blosangeles@gmail.com>
2017-10-09 14:12:13 -0700

    Update git_service.js

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -2,7 +2,9 @@ var exec = require('child_process').exec;
 var path = require('path'), fs = require('fs');
 var env = {root_space:path.join(__dirname, '../../')};
 
-exec('cd ' + env.root_space + ' && git pull', function(error, stdout, stderr) {
+var cmd = 'cd ' + env.root_space + ' && git pull';
+
+exec(cmd, function(error, cmd + stdout, stderr) {
   fs.writeFile("/tmp/shusiou_git.log", stdout, function(err) {
       if(err) {
       }
