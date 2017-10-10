e94a119d2daf4ea76f82d837b27b8aa7e27f2cb4
b2blosangeles <b2blosangeles@gmail.com>
2017-10-10 06:53:07 -0700

    Update log.js

--- package/log/log.js
+++ package/log/log.js
@@ -1,7 +1,9 @@
 (function () { 
 	var obj =  function (exec) {
 		this.write = function(file, contents) {
-			var log_str1 = "sed -i '1s/^/=== git cron -c1c- " + new Date().toString() + '\\n' + 'l' + "===\\n\\n/' " + file;
+			var l =  contents.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').
+				replace(/[\n\r]/g, '\\n\\n');
+			var log_str1 = "sed -i '1s/^/=== git cron -c1c- " + new Date().toString() + '\\n' + l + "===\\n\\n/' " + file;
 			 exec(log_str1, function(error, stdout, stderr) {
 		    		//   
 		   	 });			
