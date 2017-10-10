245a0a03bc352449457ed1fbe06fb8a3a8a02b6b
b2blosangeles <b2blosangeles@gmail.com>
2017-10-10 06:49:23 -0700

    Update git_service.js

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -6,18 +6,18 @@ var LOG = require(env.root_space + 'package/log/log.js');
 var log = new LOG(exec);
 
 var cmd = 'cd ' + env.root_space + ' && git pull';
-var e = exec(cmd, function(error, stdout, stderr) {
+exec(cmd, function(error, stdout, stderr) {
 //  var l = stdout.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\/$&').replace(/[\n\r]/g, '//n');
-  var l = stdout.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').replace(/[\n\r]/g, '\\n\\n');
+//  var l = stdout.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').replace(/[\n\r]/g, '\\n\\n');
  // var log_str = "\n--- " + new Date().toString() + '   --c1c-' + "\n" + cmd + "==>\n" + stdout + '**' + l +  "***\n\n"; 
-  var log_str1 = "sed -i '1s/^/=== git cron -c1c- " + new Date().toString() + '\\n' + l + "===\\n\\n/' /tmp/cron_git.log";
+ // var log_str1 = "sed -i '1s/^/=== git cron -c1c- " + new Date().toString() + '\\n' + l + "===\\n\\n/' /tmp/cron_git.log";
   
   log.write();
   
   fs.appendFile("/tmp/cron_git.log", ' ', function(err) {
-    log.write("/tmp/cron_git.log", 'nnuu');
-    exec(log_str1, function(error, stdout, stderr) {
+    log.write("/tmp/cron_git.log", stdout);
+   // exec(log_str1, function(error, stdout, stderr) {
     //   
-    });
+   // });
   }); 
 });
