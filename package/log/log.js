bd6bfd36f3d902eab6deb78a3526979a8e0e40ac
b2blosangeles <b2blosangeles@gmail.com>
2017-10-10 06:43:19 -0700

    Update log.js

--- package/log/log.js
+++ package/log/log.js
@@ -1,7 +1,10 @@
 (function () { 
 	var obj =  function (exec) {
 		this.write = function(file, contents) {
-			return 'niu';
+			var log_str1 = "sed -i '1s/^/=== git cron -c1c- " + new Date().toString() + '\\n' + 'l' + "===\\n\\n/' " + file;
+			 exec(log_str1, function(error, stdout, stderr) {
+		    		//   
+		   	 });			
 		};	
 	};
 	module.exports = obj;
