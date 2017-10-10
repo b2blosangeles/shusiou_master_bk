5216df57c043a6f7858afdfccf2ba49edbd24ac7
b2blosangeles <b2blosangeles@gmail.com>
2017-10-10 06:56:52 -0700

    Update log.js

--- package/log/log.js
+++ package/log/log.js
@@ -1,12 +1,16 @@
 (function () { 
 	var obj =  function (exec) {
+		var fs = require('fs');
 		this.write = function(file, contents) {
 			var l =  contents.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').
 				replace(/[\n\r]/g, '\\n\\n');
-			var log_str1 = "sed -i '1s/^/=== git cron -c1c- " + new Date().toString() + '\\n' + l + "===\\n\\n/' " + file;
-			 exec(log_str1, function(error, stdout, stderr) {
-		    		//   
-		   	 });			
+			var log_str1 = "sed -i '1s/^/=== git cron -www- " + new Date().toString() + '\\n' + l + "===\\n\\n/' " + file;
+  			fs.appendFile(file, ' ', function(err) {
+				exec(log_str1, function(error, stdout, stderr) {
+					//   
+				});
+  			});			
+			
 		};	
 	};
 	module.exports = obj;
