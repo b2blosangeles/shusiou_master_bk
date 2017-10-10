b79a0476bd2cc3c3526bda1c5f68142f4c26d981
b2blosangeles <b2blosangeles@gmail.com>
2017-10-09 17:44:47 -0700

    Update git_service.js

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -12,7 +12,7 @@ exec(cmd, function(error, stdout, stderr) {
     var log_a = stdout.split("\n");
     var l = log_a.join("\\n")
     
-    var log_str1 = "sed -i '1s/^/===" new Date().toString() + '===\\n' + l + "\\n/' /tmp/cron_git.log";
+    var log_str1 = "sed -i '1s/^/===" + new Date().toString() + '===\\n' + l + "\\n/' /tmp/cron_git.log";
     exec1(log_str1, function(error, stdout, stderr) {
 
     });
