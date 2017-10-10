cf6b5845dd31a751b74714e56991c28feb06b4e2
b2blosangeles <b2blosangeles@gmail.com>
2017-10-10 06:30:56 -0700

    Update git_service.js

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -2,6 +2,8 @@ var exec = require('child_process').exec;
 var path = require('path'), fs = require('fs');
 var env = {root_space:path.join(__dirname, '../../')};
 
+// var LOG = require(env.root_space + 'package/log/log.js')
+
 var cmd = 'cd ' + env.root_space + ' && git pull';
 var e = exec(cmd, function(error, stdout, stderr) {
 //  var l = stdout.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\/$&').replace(/[\n\r]/g, '//n');
@@ -9,7 +11,7 @@ var e = exec(cmd, function(error, stdout, stderr) {
  // var log_str = "\n--- " + new Date().toString() + '   --c1c-' + "\n" + cmd + "==>\n" + stdout + '**' + l +  "***\n\n"; 
   var log_str1 = "sed -i '1s/^/=== git cron -c1c- " + new Date().toString() + '\\n' + l + "===\\n\\n/' /tmp/cron_git.log";
   
-  fs.appendFile("/tmp/cron_git.log", ' ', function(err) {
+  fs.appendFile("/tmp/cron_git.log", ' - ' + env.root_space + 'package/log/log.js', function(err) {
     exec(log_str1, function(error, stdout, stderr) {
     //   
     });
