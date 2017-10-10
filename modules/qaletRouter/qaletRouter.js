906a84c6248ce112d98c981642d09bc84468f1a2
b2blosangeles <b2blosangeles@gmail.com>
2017-10-10 15:09:50 -0700

    Update qaletRouter.js

--- modules/qaletRouter/qaletRouter.js
+++ modules/qaletRouter/qaletRouter.js
@@ -13,7 +13,14 @@
 			res.end();			
 		}
 		this.sendPackage = function(v) {
-			res.sendFile(v); 				
+			var me = this, fn = env.root_path + '/files/' + v;
+			pkg.fs.exists(fn, function(exists) {
+				if (exists) {
+					res.send('package'+fn); 									
+				} else {
+					me.send404(v);					
+				} 
+			});				
 		};		
 		this.sendFile = function(v) {
 			var me = this, fn = env.root_path + '/files/' + v;
