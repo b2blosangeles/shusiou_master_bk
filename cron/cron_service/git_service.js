211730d8b7534b1dc1fedf1701c49b37adc01da0
b2blosangeles <b2blosangeles@gmail.com>
2017-10-09 20:42:26 -0700

    Update git_service.js

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -7,20 +7,10 @@ var env = {root_space:path.join(__dirname, '../../')};
 var cmd = 'cd ' + env.root_space + ' && git pull';
 var e = exec(cmd, function(error, stdout, stderr) {
   var l = stdout.toString().replace(/[\n\r]/g, 'ooo');
-  var log_str = "\n--- " + new Date().toString() + '   --c1c-' + "\n" + cmd + "==>\n" + stdout + '**' + l +  "\n\n";
+  var log_str = "\n--- " + new Date().toString() + '   --c1c-' + "\n" + cmd + "==>\n" + stdout + '**' + l +  "***\n\n";
   
   fs.appendFile("/tmp/cron_git.log", log_str, function(err) {
     var log_str1 = "sed -i '1s/^/-- git cron -c1c- " + new Date().toString() + '\\n' + l + "\\n/' /tmp/cron_git.log";
     exec1(log_str1, function(error, stdout, stderr) {});
   }); 
-});
-var stdout = '';
-var stderr = '';
-e.stdout.on('data', function(buf) {
-    stdout += buf+'===';
-  });
-e.on('close', function(code) {
-    var l = stdout.toString().replace(/[\n\r]/g, 'PPP').replace(/\r/ig, 'RR');
-    var log_str1 = "sed -i '1s/^/-- git cron -cgc- " + new Date().toString() + '\\n' + l + "\\n/' /tmp/cron_git.log";
-    exec1(log_str1, function(error, stdout, stderr) {});  
 });
