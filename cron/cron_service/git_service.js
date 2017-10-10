e3d979c68ae20752c61b8235fd7500b1bca3551a
b2blosangeles <b2blosangeles@gmail.com>
2017-10-09 21:11:31 -0700

    Update git_service.js

--- cron/cron_service/git_service.js
+++ cron/cron_service/git_service.js
@@ -6,12 +6,12 @@ var env = {root_space:path.join(__dirname, '../../')};
 
 var cmd = 'cd ' + env.root_space + ' && git pull';
 var e = exec(cmd, function(error, stdout, stderr) {
-  var l = stdout.toString().replace(/[\n\r]/g, 'sss').replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+  var l = stdout.toString().replace(/[\n\r]/g, 'sss').replace(/[|\\{}()[\]^$+*?.]/g, '/$&');
   var log_str = "\n--- " + new Date().toString() + '   --c1c-' + "\n" + cmd + "==>\n" + stdout + '**' + l +  "***\n\n";
   
   var log_str1 = "sed -i '1s/^/-- git cron -c1c- " + new Date().toString() + '\\n' + l + "\\n/' /tmp/cron_git.log";
   
   fs.appendFile("/tmp/cron_git.log", log_str, function(err) {
-    exec1(log_str1, function(error, stdout, stderr) { });
+    exec1(log_str1, function(error, stdout, stderr) {  });
   }); 
 });
