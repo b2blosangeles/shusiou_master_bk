e0213e0468d2ac1f9a107a13b0fc0da33a4db7fa
b2blosangeles <b2blosangeles@gmail.com>
2017-10-10 07:05:21 -0700

    Update log.js

--- package/log/log.js
+++ package/log/log.js
@@ -1,10 +1,13 @@
 (function () { 
 	var obj =  function () {
 		var fs = require('fs'), exec = require('child_process').exec;
-		this.write = function(file, contents) {
+		this.write = function(file, cmd, contents) {
 			var l =  contents.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').
 				replace(/[\n\r]/g, '\\n\\n');
-			var log_str1 = "sed -i '1s/^/=== git cron " + new Date().toString() + '\\n' + l + "===\\n\\n/' " + file;
+			var c =  cmd.toString().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '\\$&').
+				replace(/[\n\r]/g, '\\n\\n');
+			
+			var log_str1 = "sed -i '1s/^/=== git cron :: " + c + ' == ' +  + new Date().toString() + '==\\n' + l + "\\n\\n/' " + file;
   			fs.appendFile(file, ' ', function(err) {
 				exec(log_str1, function(error, stdout, stderr) {});
   			});			
