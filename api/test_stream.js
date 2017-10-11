c8c5ab7b48fb79938df0323c9563b6b245912acd
b2blosangeles <b2blosangeles@gmail.com>
2017-10-11 14:30:03 -0700

    Update test_stream.js

--- api/test_stream.js
+++ api/test_stream.js
@@ -1,6 +1,6 @@
 var fn = '/var/log/cron_git.log';
 pkg.fs.stat(fn, function(err, data) {
-	if (err) 
+	if (err) {
 		//      res.redirect('http://api.shusiou.com'+req.url);
 		res.send('NO');
 	} else {
