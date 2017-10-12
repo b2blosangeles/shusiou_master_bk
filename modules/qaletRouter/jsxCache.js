e21111f26c6a3962e203ebe0e3b1c967620054e6
b2blosangeles <b2blosangeles@gmail.com>
2017-10-12 16:05:02 -0700

    Create jsxCache.js

---
+++ modules/qaletRouter/jsxCache.js
@@ -0,0 +1,15 @@
+(function () {
+	var obj =  function (fn) {
+		this.read = function() {
+
+		}		
+		this.write = function() {
+
+		}		    
+	};
+	
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = obj;
+	} 
+	
+})();
