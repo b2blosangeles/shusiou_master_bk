c62269f5ac4d543bc5bd723aee0f73e2c48ec0db
b2blosangeles <b2blosangeles@gmail.com>
2017-10-12 10:35:58 -0700

    Update qaletRouter.js

--- modules/qaletRouter/qaletRouter.js
+++ modules/qaletRouter/qaletRouter.js
@@ -15,16 +15,10 @@
 		this.sendPackage = function(v) {
 			var me = this;
 			var fn = env.root_path + '/files/package/' + v;
-			//pkg.fs.exists(fn, function(exists) {
-			//	if (exists) {
-					delete require.cache[__dirname + '/qaletPackage.js'];
-					var router  = require(__dirname + '/qaletPackage.js');
-					var P = new router(pkg, env, req, res);						
-					P.load(fn);								
-			//	} else {
-			//		me.send404(v);					
-			//	} 
-			//});
+			delete require.cache[__dirname + '/qaletPackage.js'];
+			var router  = require(__dirname + '/qaletPackage.js');
+			var P = new router(pkg, env, req, res);						
+			P.load(fn);								
 		};		
 		this.sendFile = function(v) {
 			var me = this, fn = env.root_path + '/files/' + v;
