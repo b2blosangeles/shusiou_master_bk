1fc9cc53890b8ddd12f5e3df5579d375c0bf00c0
b2blosangeles <b2blosangeles@gmail.com>
2017-10-12 16:35:12 -0700

    Update pkCache.js

--- modules/qaletRouter/pkCache.js
+++ modules/qaletRouter/pkCache.js
@@ -1,6 +1,7 @@
 (function () {
 	var obj =  function () {
 		var folderP  = require(__dirname + '/folderP.js');
+		var fs = require('fs');
 		this.folderP = new folderP();
 		
 		this.exist = function(fn) {
