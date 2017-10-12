24feb606a6106c530408207ec18eee5a3dc9cd10
b2blosangeles <b2blosangeles@gmail.com>
2017-10-11 21:32:28 -0700

    Update api.js

--- api/admin/api.js
+++ api/admin/api.js
@@ -4,31 +4,30 @@ if (!code) {
 } else {   
    switch(opt) {
       case 'show':
+         var fn = '/var/log/'+code;
+         pkg.fs.stat(fn, function(err, data) {
+            if (err) {
+               res.send('No log file ' + code);
+            } else {         
+                  var stream = require("stream")
+                  var s = new stream.PassThrough();
+                  s.push('*** Current view time:' + new Date().toString() + " *** \n\n");
+                  s.end()	 
 
-      var fn = '/var/log/'+code;
-      pkg.fs.stat(fn, function(err, data) {
-         if (err) {
-            res.send('No log file ' + code);
-         } else {         
-               var stream = require("stream")
-               var s = new stream.PassThrough();
-               s.push('*** Current view time:' + new Date().toString() + " *** \n\n");
-               s.end()	 
+                  var readerStream1 = pkg.fs.createReadStream(fn);
 
-               var readerStream1 = pkg.fs.createReadStream(fn);
+                  s.on('end', function(){
+                            readerStream1.pipe(res, { end:false});
+                     });		
 
-               s.on('end', function(){
-                         readerStream1.pipe(res, { end:false});
-                  });		
+                  readerStream1.on('end', function(){
+                     res.end();
+                      });
 
-               readerStream1.on('end', function(){
-                  res.end();
-                   });
-
-               s.pipe(res, { end:false});
-            }
-         });
-         break;
+                  s.pipe(res, { end:false});
+               }
+            });
+            break;
       case 'remove':
          var exec = require('child_process').exec;
          var cmd = 'rm  /var/log/'+ code;
