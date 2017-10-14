d8e6a2333412e67d2e5af7a0696576d461721838
b2blosangeles <b2blosangeles@gmail.com>
2017-10-13 18:19:53 -0700

    Update pkCache.js

--- modules/qaletRouter/pkCache.js
+++ modules/qaletRouter/pkCache.js
@@ -18,7 +18,9 @@
 						me.folderP.build(p, function() {
 							fs.readdir(p, function(err, files){
 								for (var i = 0; i < files.length; i++) {
-									fs.unlink( p + files[i]);
+									if ((p + files[i]) != fn) {
+										fs.unlink( p + files[i]);
+									}		
 								 }								
 
 								me.write(fn, data, function() {
