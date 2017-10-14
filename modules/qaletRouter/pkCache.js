d12d3b13e2bff75a969ef1b7b373e2fa89484a73
b2blosangeles <b2blosangeles@gmail.com>
2017-10-13 19:06:24 -0700

    Update pkCache.js

--- modules/qaletRouter/pkCache.js
+++ modules/qaletRouter/pkCache.js
@@ -24,7 +24,7 @@
 									}		
 								 }								
 								me.write(p + key, data, function() {
-									callback(data + '==' + fn);
+									callback(data);
 								});							
 							});
 							
