e19c87bc9845eb02123d953516df3376bb4ac7e9
b2blosangeles <b2blosangeles@gmail.com>
2017-10-13 20:50:13 -0700

    Update pkCache.js

--- modules/qaletRouter/pkCache.js
+++ modules/qaletRouter/pkCache.js
@@ -4,7 +4,7 @@
 		var fs = require('fs');
 		this.folderP = new folderP();
 		
-		this.exist = function(fn, key, callback, trans) {
+		this.exist = function(fn, key, callback, qaletBabel) {
 			var me = this;
 			var patt = new RegExp('^' + root_path + '/files/');
 			var p = '/tmp/cache/'+ fn.replace(patt, '').replace(/\//g, '_')+'/';
@@ -14,6 +14,29 @@
 						callback(data + '==cached==');
 					});
 				} else {
+					qaletBabel.jsx2js(fn, function(err, v) {
+						if (err) {
+							callback('console.log("'+err.message.replace('"', '')+'");');
+						} else {
+							me.folderP.build(p, function() {
+								fs.readdir(p, function(err, files){
+
+									for (var i = 0; i < files.length; i++) {
+										if ((p + files[i]) != fn) {
+											fs.unlink( p + files[i]);
+										}		
+									 }								
+									me.write(p + key, v.code, function() {
+										callback(v.code);
+									});							
+								});
+
+							});							
+							callback(v.code + '===v.code');
+						}   
+					});
+					/*
+					
 					me.read(fn, function(data) {
 						if (typeof trans == 'function') {
 							data = trans(data);
@@ -33,6 +56,7 @@
 							
 						});
 					});
+					*/
 				}
 			});
 		}		
