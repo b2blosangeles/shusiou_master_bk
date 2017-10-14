0d3cad7ae06b055f6b39fe214844309003e3dd8e
b2blosangeles <b2blosangeles@gmail.com>
2017-10-13 22:29:19 -0700

    Update test_stream_video.js

--- api/test_stream_video.js
+++ api/test_stream_video.js
@@ -1,7 +1,7 @@
 var file_video = env.root_path + '/api/SampleVideo_1280x720_5mb.mp4';
 var folder_image = 'tmp/images/';
 
-var fn = 'tmp/images/1.png',
+var fn = 'tmp/images/1.png', s=10;
     
 var CP = new pkg.crowdProcess();
 res.send(fn);
@@ -60,27 +60,3 @@ CP.serial(
 	},
 	6000
 );
-
-pkg.fs.stat(fn, function(err, data) {
-    if (err) 
-      res.redirect('http://api.shusiou.com'+req.url);
-    else {
-	      var total = data.size;
-	      var range = req.headers.range;
-	      if (range) {
-			var parts = range.replace(/bytes=/, "").split("-");
-			var partialstart = parts[0]; var partialend;
-			  partialend =  parts[1];
-			var start = parseInt(partialstart, 10);
-			var end = partialend ? parseInt(partialend, 10) : total-1;
-			var chunksize = (end-start)+1;
-			var file = pkg.fs.createReadStream(fn, {start:start, end:end});
-
-			res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
-				'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-		       file.pipe(res);
-		} else {
-			res.send('Need streaming player');
-		}
-	}
-});
