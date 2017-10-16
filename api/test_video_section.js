985e72f222241157996402b612497927cf82fb0a
b2blosangeles <b2blosangeles@gmail.com>
2017-10-16 07:45:53 -0700

    Update test_video_section.js

--- api/test_video_section.js
+++ api/test_video_section.js
@@ -3,6 +3,10 @@ var s_file = env.root_path + '/api/SampleVideo_1280x720_5mb.mp4',  s = 1, l =  6
 var fn = c_folder + s + '_' + l + '.mp4';
 
 
+		
+res.send(env.root_pat + '/api/inc/folderP/folderP);
+			return true;
+
 var childProcess = require('child_process');
 var CP = new pkg.crowdProcess();
 var _f = {};
