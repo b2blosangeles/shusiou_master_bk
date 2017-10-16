5e2599a8b2d2b07b3c14782aaeb15dcc69759465
b2blosangeles <b2blosangeles@gmail.com>
2017-10-16 08:12:47 -0700

    Update test_video_section.js

--- api/test_video_section.js
+++ api/test_video_section.js
@@ -1,5 +1,5 @@
 var c_folder =  '/tmp/tmp_section/';
-var s_file = env.root_path + '/api/SampleVideo_1280x720_5mb.mp4',  s = 1, l =  6;
+var s_file = env.root_path + '/api/SampleVideo_1280x720_5mb.mp4',  s = 31, l =  61;
 var fn = c_folder + s + '_' + l + '.mp4';
 
 
