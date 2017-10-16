749df4916d6f58b5de914b84f8d0d48518176570
b2blosangeles <b2blosangeles@gmail.com>
2017-10-16 13:31:41 -0700

    Update test_download.js

--- api/test_download.js
+++ api/test_download.js
@@ -2,7 +2,7 @@ var ytdl = require(env.root_path + '/api/inc/ytdl-core/node_modules/ytdl-core');
 var mysql = require(env.root_path + '/api/inc/mysql/node_modules/mysql');
 
 //var video = ytdl(url, {range: {start:start, end:end}, quality:'18'});
-var url = 'https://youtu.be/SjF4TXpqatc';
+var url = 'https://youtu.be/wLOl00FJMtc';
 var video = ytdl(url, {quality:'18'});
 video.pipe(pkg.fs.createWriteStream('/tmp/niu.mp4'));	
 
