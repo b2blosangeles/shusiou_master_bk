279fa8c992d2f5e5e0815c9628111b5f3e679bf6
b2blosangeles <b2blosangeles@gmail.com>
2017-10-16 16:04:32 -0700

    Update cron_download.js

--- api/cron_download.js
+++ api/cron_download.js
@@ -13,7 +13,8 @@ _f['P0'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
 	
-	var str = 'SELECT * FROM `download_queue` WHERE `id` NOT IN  (SELECT `id` FROM `download_queue` WHERE 1) ';
+	var str = 'SELECT * FROM `download_queue` WHERE `id` NOT IN ' +
+	    ' (SELECT `id` FROM `download_queue` WHERE 1) ';
 			//	'values ("' + source + '", "' + encodeURIComponent(code) + '", "' + uid + '", NOW(), 0 ); ';
 	connection.query(str, function (error, results, fields) {
 		connection.end();
