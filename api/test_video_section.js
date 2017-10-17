fbe2485aa1dd96677c784d454185526b1fd63329
b2blosangeles <b2blosangeles@gmail.com>
2017-10-17 14:52:07 -0700

    Update test_video_section.js

--- api/test_video_section.js
+++ api/test_video_section.js
@@ -1,7 +1,8 @@
 var c_folder =  '/tmp/tmp_section/';
 var s_file = env.root_path + '/api/SampleVideo_1280x720_5mb.mp4',  s = 1, l =  81;
+s_file =  '/tmp/' + encodeURIComponent('https://youtu.be/phpT_yukNks')+'.mp4';
 var fn = c_folder + s + '_' + l + '.mp4';
-
+var fn = c_folder + 'niu.mp4';
 
 var childProcess = require('child_process');
 var CP = new pkg.crowdProcess();
