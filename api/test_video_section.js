2fb49ccce3a0da7d80ca785cfbde366e1a09915f
b2blosangeles <b2blosangeles@gmail.com>
2017-10-17 15:04:12 -0700

    Update test_video_section.js

--- api/test_video_section.js
+++ api/test_video_section.js
@@ -1,5 +1,5 @@
 var c_folder =  '/tmp/tmp_section/';
-var s_file = env.root_path + '/api/SampleVideo_1280x720_5mb.mp4',  s = 10, l =  6;
+var s_file = env.root_path + '/api/SampleVideo_1280x720_5mb.mp4',  s = 10, l =  0;
 s_file =  '/tmp/' + encodeURIComponent('https://youtu.be/phpT_yukNks')+'.mp4';
 var fn = c_folder + s + '_' + l + '.mp4';
 
