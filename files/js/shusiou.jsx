7cc33aba52ae676227b37c6977b80480364e6bc5
b2blosangeles <b2blosangeles@gmail.com>
2017-10-18 21:41:39 -0700

    Update shusiou.jsx

--- files/js/shusiou.jsx
+++ files/js/shusiou.jsx
@@ -1,1 +1,14 @@
-console.log('===niu23===');
+function setVideo(url, s, l) {
+var r = '' +
+'<video width="640" height="480" controls autoplay>' +
+	 ' <source src="'+url+'?s='+s+'&l='+l+'" type="video/mp4">' +
+	'  <source src="movie.ogg" type="video/ogg">' +
+'	Your browser does not support the video tag.' +
+	'</video>';
+  document.getElementById('niu').innerHTML = r;
+}
+
+setTimeout(
+ setVideo('/api/test_video_section.js', 10, 6) {
+ }, 1000
+)
