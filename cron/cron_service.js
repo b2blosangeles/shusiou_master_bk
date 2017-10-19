656d601d81f9a14ef5813290fddb8440a86b0939
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 08:04:16 -0700

    Update cron_service.js

--- cron/cron_service.js
+++ cron/cron_service.js
@@ -3,7 +3,7 @@ var manager = new CronJobManager();
 var exec = require('child_process').exec;
 var fs    = require('fs'), path = require('path');
 var root_path =  path.join(__dirname, '..');
-console.log('--->' + root_path);
+
 var LOG = require(root_path + '/package/log/log.js');
 var log = new LOG();
 
@@ -17,9 +17,9 @@ fs.exists(conf_file, function(exists){
 				return function() {
 					exec('cd ' + root_path + '/site/cron' + ' &&  ' + v.script, function(error, stdout, stderr) {
 						if (!stderr) {
-							console.log(JSON.stringify({status:'success', id:v.id, message:stdout}));
+							log.write("/var/log/shusiou_cron.log", JSON.stringify({status:'success', id:v.id, message:stdout}));
 						} else {
-							console.log(JSON.stringify({status:'error', id:v.id, message:stderr}));
+							log.write("/var/log/shusiou_cron.log", JSON.stringify({status:'error', id:v.id, message:stderr}));
 						}
 					});
 
