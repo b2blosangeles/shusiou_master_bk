6ef96bde224d904d5f2425545484a378673d24ce
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 08:12:27 -0700

    Update cron_service.js

--- cron/cron_service.js
+++ cron/cron_service.js
@@ -11,35 +11,39 @@ var conf_file = root_path + '/site/cron/cron.json';
 
 fs.exists(conf_file, function(exists){
 	if(exists) {
-		var cron = require(conf_file);
-		for (var i = 0; i < cron.length; i++) {
-			var f = function(v) {
-				return function() {
-					exec('cd ' + root_path + '/site/cron' + ' &&  ' + v.script, function(error, stdout, stderr) {
-						if (!stderr) {
-							log.write("/var/log/shusiou_cron.log", 'cron', JSON.stringify({status:'success', id:v.id, message:stdout}));
-						} else {
-							log.write("/var/log/shusiou_cron.log", 'cron', JSON.stringify({status:'error', id:v.id, message:stderr}));
-						}
-					});
+		try {
+			var cron = require(conf_file);
+			for (var i = 0; i < cron.length; i++) {
+				var f = function(v) {
+					return function() {
+						exec('cd ' + root_path + '/site/cron' + ' &&  ' + v.script, function(error, stdout, stderr) {
+							if (!stderr) {
+								log.write("/var/log/shusiou_cron.log", 'cron', JSON.stringify({status:'success', id:v.id, message:stdout}));
+							} else {
+								log.write("/var/log/shusiou_cron.log", 'cron', JSON.stringify({status:'error', id:v.id, message:stderr}));
+							}
+						});
 
+					}
+				};
+				if (manager.exists( cron[i]['id'])) {
+					manager.stop( cron[i]['id']);
 				}
-			};
-			if (manager.exists( cron[i]['id'])) {
-				manager.stop( cron[i]['id']);
+
+
+				if (cron[i].script) {
+					if (!manager.exists( cron[i]['id'])) {
+
+						manager.add( cron[i]['id'], cron[i]['schedule'], f(cron[i]), null, false, "America/Los_Angeles");
+					} else {
+						manager.deleteJob( cron[i]['id']);
+					}
+					manager.start( cron[i]['id']);
+				}	
 			}
-
-
-			if (cron[i].script) {
-				if (!manager.exists( cron[i]['id'])) {
-
-					manager.add( cron[i]['id'], cron[i]['schedule'], f(cron[i]), null, false, "America/Los_Angeles");
-				} else {
-					manager.deleteJob( cron[i]['id']);
-				}
-				manager.start( cron[i]['id']);
-			}	
-		}		
+		} catch (e) {
+			log.write("/var/log/shusiou_cron.log", 'cron', 'cron.json format error!');
+		}
 	} else {
 		log.write("/var/log/shusiou_cron.log", 'cron', 'Missing cron.json!');
 	}
