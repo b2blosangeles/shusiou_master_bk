55519f7dd02b6fbc25ae5a6be4ae5bd98c81e224
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 07:51:52 -0700

    Update cron_service.js

--- cron/cron_service.js
+++ cron/cron_service.js
@@ -37,36 +37,6 @@ fs.exists(conf_file, function(exists){
 			}	
 		}		
 	} else {
-		console.log('Missing ' + cron.json);
+		console.log('Missing cron.json!');
 	}
 });
-/*
-for (var i = 0; i < cron.length; i++) {
-	var f = function(v) {
-		return function() {
-			exec('cd ' + __dirname + '/site/cron &&  ' + v.script, function(error, stdout, stderr) {
-				if (!stderr) {
-					console.log(JSON.stringify({status:'success', id:v.id, message:stdout}));
-				} else {
-					console.log(JSON.stringify({status:'error', id:v.id, message:stderr}));
-				}
-			});
-			
-		}
-	};
-	if (manager.exists( cron[i]['id'])) {
-		manager.stop( cron[i]['id']);
-	}
-	
-	
-	if (cron[i].script) {
-		if (!manager.exists( cron[i]['id'])) {
-			
-			manager.add( cron[i]['id'], cron[i]['schedule'], f(cron[i]), null, false, "America/Los_Angeles");
-		} else {
-			manager.deleteJob( cron[i]['id']);
-		}
-		manager.start( cron[i]['id']);
-	}	
-}
-*/
