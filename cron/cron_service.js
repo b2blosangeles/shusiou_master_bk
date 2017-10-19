5dc5cf684a0406ffeafdae00699e0eeb99cf2d5a
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 07:37:43 -0700

    Update cron_service.js

--- cron/cron_service.js
+++ cron/cron_service.js
@@ -1,7 +1,12 @@
 var CronJobManager = require('./crontab_manager.js');
 var manager = new CronJobManager();
 var exec = require('child_process').exec;
-var cron = require('./cron.json'), fs    = require('fs')
+var fs    = require('fs')
+var cron = require('./cron.json');
+console.log(__dirname + "===");
+fs.exists('./cron.json',function(exists){
+  // handle result
+});
 
 for (var i = 0; i < cron.length; i++) {
 	var f = function(v) {
